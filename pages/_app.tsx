1fa72ddba1704e2f0d7be13feb840908ff4cf6e2
coderinblack <kevinlu.email@gmail.com>
2020-11-01 19:49:30 -0800

    SEO and sitemap.xml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
-import * as React from 'react';
+import React from 'react';
+import Head from 'next/head';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <div className="antialiased">
+      <Head>
+        <title>Coderinblack</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Coderinblack portfolio, personal website"
+        />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+          name="google-site-verification"
+          content="WaNom3LvA5kHdHHd-Kpo-6aj0_7JEEs1_T456egbbVI"
+        />
+        <meta
+          name="keywords"
+          content="coderinblack, software, coding, react, vue, nextjs, typescript, pluto"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
